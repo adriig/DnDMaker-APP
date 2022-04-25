4473274e3f58d62904cb22ee6706bd7b80ee34b0
adriig <adriagarciapons3@gmail.com>
2022-04-25 21:55:30 +0200

    Agregado routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { LoginComponent } from './components/login/login.component';
 
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SharedModule } from './shared-modules/shared/shared.module';
+import { SharedModule } from './shared-modules/shared.module';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
